b5fea69c5408c736094ef589af7c94a4786473d7
sankuya <50142065+sankuya@users.noreply.github.com>
2021-05-25 23:55:51 +0800

    配置生产环境、开发环境分离

--- src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
+++ src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:profile.properties")
+@PropertySource("profile.properties")
 public class NebulaBlogRefactorApplication {
 
     public static void main(String[] args) {
