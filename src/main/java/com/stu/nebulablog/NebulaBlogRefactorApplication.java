64d45f3a10f62476ef6953cd85b549e57d0fde9a
sankuya <50142065+sankuya@users.noreply.github.com>
2021-05-26 20:27:21 +0800

    增加日志

--- src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
+++ src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("profile.properties")
+@PropertySource("classpath:/profiles/${spring.profiles.active}/profile.properties")
 public class NebulaBlogRefactorApplication {
 
     public static void main(String[] args) {
