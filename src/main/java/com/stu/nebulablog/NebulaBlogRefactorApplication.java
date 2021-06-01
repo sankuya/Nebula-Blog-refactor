cb0b8517be630cb47ab92728b38e62bc1f720df9
sankuya <50142065+sankuya@users.noreply.github.com>
2021-06-02 00:37:04 +0800

    debug

--- src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
+++ src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
@@ -2,6 +2,7 @@ package com.stu.nebulablog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class NebulaBlogRefactorApplication {
