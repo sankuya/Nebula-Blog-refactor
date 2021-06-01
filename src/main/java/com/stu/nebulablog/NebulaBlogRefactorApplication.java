d3f4784d227ee33aeefb42a1e95a2f4aa8b3a88c
sankuya <50142065+sankuya@users.noreply.github.com>
2021-06-01 22:54:28 +0800

    将配置文件移到jar包外

--- src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
+++ src/main/java/com/stu/nebulablog/NebulaBlogRefactorApplication.java
@@ -2,10 +2,8 @@ package com.stu.nebulablog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:/profiles/${spring.profiles.active}/profile.properties")
 public class NebulaBlogRefactorApplication {
 
     public static void main(String[] args) {
