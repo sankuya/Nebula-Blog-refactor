98076478ec0c1043370603d18d6f179f05903656
sankuya <50142065+sankuya@users.noreply.github.com>
2021-06-28 15:48:03 +0800

    增加文件搜索功能

--- src/main/java/com/stu/nebulablog/controller/PublicFileController.java
+++ src/main/java/com/stu/nebulablog/controller/PublicFileController.java
@@ -2,6 +2,7 @@ package com.stu.nebulablog.controller;
 
 import com.stu.nebulablog.module.ResponseData;
 import com.stu.nebulablog.service.file.SharedFileInfoListService;
+import com.stu.nebulablog.service.file.SharedFileInfoSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,11 +17,27 @@ import java.util.Map;
 public class PublicFileController {
     @Autowired
     private SharedFileInfoListService sharedFileInfoListService;
-
+    @Autowired
+    private SharedFileInfoSearchService sharedFileInfoSearchService;
+    private static final int MAXSIZE=10;
     @GetMapping("list")
     public ResponseData list(@RequestParam int page, @RequestParam int size) {
         ResponseData responseData = new ResponseData();
+        size=Math.min(MAXSIZE,size);
         responseData.setData(sharedFileInfoListService.listSharedFileInfo(page, size));
+        if (((List)((Map)responseData.getData()).get("detail")).size()!= 0) {
+            responseData.setCode(900);
+        } else {
+            responseData.setCode(901);
+            responseData.setMsg("获取失败");
+        }
+        return responseData;
+    }
+    @GetMapping("search")
+    public ResponseData search(@RequestParam int page, @RequestParam int size,@RequestParam String keyword) {
+        ResponseData responseData = new ResponseData();
+        size=Math.min(MAXSIZE,size);
+        responseData.setData(sharedFileInfoSearchService.searchSharedFileInfo(page,size,keyword));
         if (((List)((Map)responseData.getData()).get("detail")).size()!= 0) {
             responseData.setCode(900);
         } else {
--- src/main/java/com/stu/nebulablog/service/file/SharedFileInfoSearchService.java
+++ src/main/java/com/stu/nebulablog/service/file/SharedFileInfoSearchService.java
@@ -1,7 +1,40 @@
 package com.stu.nebulablog.service.file;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.stu.nebulablog.mapper.FileInfoMapper;
+import com.stu.nebulablog.module.entity.FileInfo;
+import com.stu.nebulablog.utils.PageToMapUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class SharedFileInfoSearchService {
+    @Autowired
+    private FileInfoMapper fileInfoMapper;
+    @Autowired
+    private PageToMapUtil<FileInfo> fileInfoPageToMapUtil;
+
+    public Map<String, Object> searchSharedFileInfo(int page, int size, String keyword) {
+        Page<FileInfo> fileInfoPage = new Page<>(page, size);
+        fileInfoMapper.selectPage(fileInfoPage,
+                new LambdaQueryWrapper<FileInfo>()
+                        .like(FileInfo::getFilename, keyword)
+                        .or()
+                        .like(FileInfo::getUsername, keyword)
+        );
+        Map<String, Object> res = fileInfoPageToMapUtil.getMapFromPageWithPages(fileInfoPage);
+        List<FileInfo> fileInfos = (List<FileInfo>) res.get("detail");
+        List<String> paths = new ArrayList<>();
+        fileInfos.forEach(file ->
+                paths.add(Paths.get("user", file.getUsername(), "share", file.getFilename()).toString())
+        );
+        res.put("detail", paths);
+        return res;
+    }
 }
